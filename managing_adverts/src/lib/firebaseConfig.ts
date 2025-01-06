f23394ec26bebbf83671004b0da9c6afbcf68e07
amCtrld <amctrld@gmail.com>
2025-01-06 16:06:10 +0300

    Credentials Removal

--- managing_adverts/src/lib/firebaseConfig.ts
+++ managing_adverts/src/lib/firebaseConfig.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+// Using environment variables for Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBgtakbHVy1Leube2cOK2DqT-n8N7qXUYw",
-    authDomain: "advert-43252.firebaseapp.com",
-    projectId: "advert-43252",
-    storageBucket: "advert-43252.firebasestorage.app",
-    messagingSenderId: "236536300034",
-    appId: "1:236536300034:web:11c6c54f75bd509b8b5d1b",
-    measurementId: "G-DJ2DW0PP2V"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
